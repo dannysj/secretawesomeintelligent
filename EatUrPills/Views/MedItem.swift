e98b6b206318546361b7105227310cb428c8a8de
dannysj <csji4202@hotmail.com>
2018-02-24 12:39:22 -0600

    Test

--- EatUrPills/Views/MedItem.swift
+++ EatUrPills/Views/MedItem.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class MedItem: UIView {
     
     var medicine: Medicine!
@@ -18,24 +17,43 @@ class MedItem: UIView {
         self.medicine = medicine
     }
     
-
-
+    func checkStatus() {
+        // TODO:
+        
+    }
+    
     override func draw(_ rect: CGRect) {
         // Drawing code
+        var path = UIBezierPath()
         switch medicine.shapeType {
         case ShapeType.Oval:
-            
+            path = UIBezierPath(roundedRect: rect, cornerRadius: rect.height / 2.0)
             break
         case ShapeType.Round:
-            
+            path = UIBezierPath(ovalIn: rect)
             break
         
         case ShapeType.Triangle:
+            path = UIBezierPath()
+            path.lineCapStyle = .round
+            path.lineJoinStyle  = .round
+            path.move(to: CGPoint(x: rect.width / 2.0, y: 0))
+            path.addLine(to: CGPoint(x: rect.width , y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            path.addLine(to: CGPoint(x: rect.width / 2.0, y: 0))
+            path.close()
             
             break
         default:
-            <#code#>
+            fatalError()
         }
+        
+        // fill
+        UIColor(hex: medicine.color).setFill()
+        UIColor.white.setStroke()
+        
+        path.fill()
+        path.stroke()
     }
     
 
