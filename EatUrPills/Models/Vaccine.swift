05427f7666100848472829e66be19f63b15762f6
dannysj <csji4202@hotmail.com>
2018-02-24 10:38:42 -0600

    Updating Vaccine Model

---
+++ EatUrPills/Models/Vaccine.swift
@@ -0,0 +1,21 @@
+//
+//  Vaccine.swift
+//  EatUrPills
+//
+//  Created by Danny Chew on 2/24/18.
+//  Copyright © 2018 Danny Chew. All rights reserved.
+//
+
+import Foundation
+
+struct Vaccine {
+    var name: String!
+    var takenDate: Date!
+    var month: Int!
+    
+    init(name: String, takenDate: Date, month: Int) {
+        self.name = name
+        self.takenDate = takenDate
+        self.month = month
+    }
+}
