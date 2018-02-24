9ed685929b6348613db49f9fb04d87fb1aa15a7d
dannysj <csji4202@hotmail.com>
2018-02-24 10:36:55 -0600

    Updating Medicine Model

--- EatUrPills/Models/Medicine.swift
+++ EatUrPills/Models/Medicine.swift
@@ -8,16 +8,26 @@
 
 import Foundation
 
+enum ShapeType {
+    case Triangle
+    case Round
+    case Oval
+}
+
 struct Medicine {
     var name:String!
     var number: Int!
     var description: String!
     var imageURL: String!
+    var color:Int!
+    var shapeType: ShapeType!
     
-    init(name: String, number: Int, description: String, imageURL: String) {
+    init(name: String, number: Int, description: String, imageURL: String, color:Int, shapeType: ShapeType) {
         self.name = name
         self.number = number
         self.description = description
         self.imageURL = imageURL
+        self.color = color
+        self.shapeType = shapeType
     }
 }
